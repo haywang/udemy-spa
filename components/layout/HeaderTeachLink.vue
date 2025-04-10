258b4120187125e02108f232b30ec103a531c9bd
“Austin” <30918019+haywang@users.noreply.github.com>
2025-04-10 13:02:19 +0800

    remove unuse file

--- components/layout/HeaderTeachLink.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <a
-    href="/teaching"
-    class="hover:text-primary-400 hover:bg-primary-100 flex h-full items-center px-4 py-2 text-sm text-gray-500"
-  >
-    Teach on Udemy
-  </a>
-</template>
